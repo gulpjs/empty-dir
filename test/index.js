63402b1fd029afdd2c41675b096b94a8455e6381
Tyler Kellen <tyler@sleekcode.net>
2014-05-08 20:37:34 -0400

    New: Initial implementation

---
+++ test/index.js
@@ -0,0 +1,20 @@
+const emptyDir = require('../');
+const expect = require('chai').expect;
+
+describe('emptyDir', function () {
+  it('should call back with true if a directory is empty', function (done) {
+    expect(emptyDir.sync('./')).to.be.false;
+    emptyDir('./', function (err, result) {
+      expect(result).to.be.false;
+      done();
+    });
+  });
+
+  it('should call back with false if a directory is not empty', function (done) {
+    expect(emptyDir.sync('./test/empty')).to.be.true;
+    emptyDir('./test/empty', function (err, result) {
+      expect(result).to.be.true;
+      done();
+    });
+  });
+});
