e0e894f7065ffa9a907830cf833cd7d85ae37860
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-25 06:05:28 -0500

    adds support for filter function
    to provide control over eliminating files like `.DS_Store` on mac or `Thumbs.db`
    on windows from causing an unexpected result. Also:
    
    - adds `exists` function, since directories that do not exist should return `false`.
    - updated metadata in package.json per latest npm recommendations and conventions
    - unit tests for filter function
    - docs for filter function
    
    closes gh-1

--- test/index.js
+++ test/index.js
@@ -9,16 +9,24 @@ try {
 describe('emptyDir', function () {
   it('should call back with true if a directory is empty', function (done) {
     expect(emptyDir.sync('./')).to.be.false;
-    emptyDir('./', function (err, result) {
-      expect(result).to.be.false;
+    emptyDir('./', function (err, empty) {
+      expect(empty).to.be.false;
+      done();
+    });
+  });
+
+  it('should be false if a directory does not exist', function (done) {
+    expect(emptyDir.sync('./foo/bar/baz')).to.be.false;
+    emptyDir('./foo/bar/baz', function (err, empty) {
+      expect(empty).to.be.false;
       done();
     });
   });
 
   it('should call back with false if a directory is not empty', function (done) {
     expect(emptyDir.sync('./test/empty')).to.be.true;
-    emptyDir('./test/empty', function (err, result) {
-      expect(result).to.be.true;
+    emptyDir('./test/empty', function (err, empty) {
+      expect(empty).to.be.true;
       done();
     });
   });
