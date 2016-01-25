8d40841c6406078b23601a32d6f13383cb43a94c
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-25 06:05:28 -0500

    adds support for filter function
    to provide control over eliminating files like `.DS_Store` on mac or `Thumbs.db` on windows from causing an unexpected result. Also:
    
    - adds `exists` function, since directories that do not exist should return `false`.
    - updated metadata in package.json per latest npm recommendations and conventions
    - unit tests for filter function
    - docs for filter function

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
+const tryOpen = require('try-open');
 
-function isEmpty (path, callback) {
-  fs.readdir(path, function (err, files) {
+function isEmpty(path, fn, callback) {
+  if (arguments.length === 2) {
+    callback = fn;
+    fn = null;
+  }
+
+  if (!exists(path)) {
+    callback(null, false);
+    return;
+  }
+
+  fs.readdir(path, function(err, files) {
     if (err === null) {
-      callback(null, !!!files.length)
+      callback(null, hasFiles(files, fn));
     } else {
       callback(err);
     }
   });
 };
 
-isEmpty.sync = function (path) {
-  var files = fs.readdirSync(path);
-  return !!!files.length;
+isEmpty.sync = function(path, fn) {
+  if (!exists(path)) {
+    return false;
+  }
+  try {
+    return hasFiles(fs.readdirSync(path), fn);
+  } catch (err) {};
+  return false;
 };
+
+function hasFiles(files, fn) {
+  return files.filter(fn || function(path) {
+    return !/\.DS_Store/i.test(path);
+  }).length === 0;
+}
+
+function exists(path) {
+  return path ? typeof tryOpen(path, 'r') === 'number' : false;
+}
 
 module.exports = isEmpty;
